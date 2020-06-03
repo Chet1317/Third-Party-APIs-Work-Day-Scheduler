696a0015ad2e4e75f1b19a376993d52ea891fb40
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-03 17:34:39 -0400

    restructured code and built for loop to get proper color coding and time slots

--- style.js
+++ style.js
@@ -1,18 +1,39 @@
-var currentDay = moment().format("H");
+var currentDay = new Date().getHours();
+document.getElementById("currentDay").innerHTML = currentDay
 var textArea = document.querySelector(".textarea");
 var saveButton = document.querySelector(".saveBtn");
-
+var containerEl = document.querySelector(".container")
 var hours = [
-    {hour: "9am", number: 9},
-    {hour:"10am", number:10},
-    {hour: "11am", number: 11},
-    {hour:"12pm", number:12},
-    {hour: "1pm", number: 13},
-    {hour:"2pm", number:14},
-    {hour: "3pm", number: 15},
-    {hour:"4pm", number:16},
-    {hour: "5pm", number: 17},
-    {hour:"6pm", number:18},
-]; 
+    {hour: "9 am", number: 9 },
+    {hour:"10 am", number:10 },
+    {hour: "11 am", number: 11 },
+    {hour:"12 pm", number:12 },
+    {hour: "1 pm", number: 13 },
+    {hour:"2 pm", number:14 },
+    {hour: "3 pm", number: 15 },
+    {hour:"4 pm", number:16 },
+    {hour: "5 pm", number: 17 },
+];
 
+for(var i=0; i < hours.length; i++ ){
+var hour = hours[i].hour;
+var numbers = Number(hours[i].number); 
+containerEl.innerHTML +=
+`<div class = "row">
+    <div class = "hour col">${hour}</div>
+    <textarea class = "col-8" data-idx="" id = "${numbers}"></textarea>
+    <button type = "col button" onClick="saveWork(${numbers})" class = "saveBtn"><i class = "far fa-save"></i></button>
+    </div>`;
+
+    if(currentDay == numbers){
+        console.log("this is present");
+        $("#" + numbers).addClass("present");
+    }else if(currentDay > numbers){
+        console.log("This is past hour");
+        $("#" + numbers).addClass("past");
+    }else {
+        console.log("This is future time");
+        $("#" + numbers).addClass("future");
+    }
+}
 
