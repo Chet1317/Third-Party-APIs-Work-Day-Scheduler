64d29ebc570ba931b249e51073b61a71721d6e21
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-03 13:57:26 -0400

    restructed code

--- style.js
+++ style.js
@@ -1,3 +1,18 @@
-var day = moment();
-document.getElementById("currentDay").innerHTML = day
+var currentDay = moment().format("H");
+var textArea = document.querySelector(".textarea");
+var saveButton = document.querySelector(".saveBtn");
+
+var hours = [
+    {hour: "9am", number: 9},
+    {hour:"10am", number:10},
+    {hour: "11am", number: 11},
+    {hour:"12pm", number:12},
+    {hour: "1pm", number: 13},
+    {hour:"2pm", number:14},
+    {hour: "3pm", number: 15},
+    {hour:"4pm", number:16},
+    {hour: "5pm", number: 17},
+    {hour:"6pm", number:18},
+]; 
+
 
